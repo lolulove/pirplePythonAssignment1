058eae59ab68bc1c4ef3468ccfb73da66cac19c3
lolulove <32493664+lolulove@users.noreply.github.com>
2019-11-19 21:22:16 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+"""
+Title: Homework Assignment #1: Variables
+
+Below contains details/attributes of my favorite song.
+All of these different characteristics could be use to describe the song. 
+
+"""
+
+# the title of my favorite song
+Title = "My Heart Will Trust In You"
+Artist = "Reuben Morgan"
+FeaturedArtist = "Darlene Zschech"
+Album = "Shout to the Lord 2000"
+ReleasedYear = 1999
+Genre = "Christian"
+DurationInSeconds = 358
+# Below is the total word count in English, other languages word count varies
+lyricWordCount = 500
+RatingStar = 5
+SongMood = "Worship"
+SongStyle = "Gospel"
+# The producer doubles as the featured artist
+Producer = "Darlene Zschech"
+Label = "Hillsong Music"
+SongWriter = "Reuben Morgan"
+# Below is the total copies sold in US and canada, including other countries is far above this
+CopiesSold = 10000000
+PublishMusicKey = "C Major"
+MusicReleasedDate = "October 25, 1999"
+MusicRecordedDate = "September 25, 1999"
+# Below is the retail price in US and canada, price varies in other countries
+CDAlbumPriceInDollars = 9.75
+# Below is the total revenue made in the United State and Canada
+AlbumRevenueInDollars = 99999999.45	
+
+print(Title)
+print(Artist)
+print(FeaturedArtist)
+print(Album)
+print(ReleasedYear)
+print(Genre)
+print(Duration)
+print(lyricWordCount)
+print(Rating)
+print(SongMood)
+print(SongStyle)
+print(Producer)
+print(Label)
+print(SongWriter)
+print(CopiesSold)
+print(PublishMusicKey)
+print(MusicReleasedDate)
+print(MusicRecordedDate)
+print(CDAlbumPriceInDollars)
+print(AlbumRevenueInDollars)
