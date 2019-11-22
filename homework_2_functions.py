16c5925298ab90a28cd5d3ae104bc181add98027
lolulove <32493664+lolulove@users.noreply.github.com>
2019-11-22 22:07:54 +0100

    Create homework_2_functions.py

---
+++ homework_2_functions.py
@@ -0,0 +1,16 @@
+def title():
+	Title = "My Heart Will Trust In You"
+	return Title
+
+def genre():
+	Genre = "Christian"
+	return Genre
+
+def releasedYear():
+	ReleasedYear = 1999
+	return ReleasedYear
+
+def favorite():
+	Favorite = True
+	return Favorite
+
